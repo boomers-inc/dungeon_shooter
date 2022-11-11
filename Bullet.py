dc301c92350fc43ff61440d07fb404a93e18bd15
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-11-10 23:04:52 -0300

    Básico para inicio do curso

--- Bullet.py
+++
@@ -1,9 +0,0 @@
-from GameObject import GameObject
-
-
-class Bullet(GameObject):
-    def __init__(self, x, y, speed, sprite, tag, damage=20, ):
-        super().__init__(x, y, speed, sprite)
-
-        self.tag = tag
-        self.damage = damage
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from GameNext import Game
+from Game import Game
 
 
 if __name__ == '__main__':
